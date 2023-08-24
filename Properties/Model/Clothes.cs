65a0e680f63a00eff91ad0a9e23bfce22a654910
Mijael Yen Quispe <mijael.yq2002@outlook.com>
2023-08-23 20:24:35 -0500

    Create Clothes.cs

---
+++ Properties/Model/Clothes.cs
@@ -0,0 +1,10 @@
+﻿namespace taller_nro1.Properties.Model;
+
+public class Clothes
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public double Price { get; set; }
+    public char Size { get; set; }
+}
